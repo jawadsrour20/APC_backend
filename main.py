306e97fb9f55fa6ff7c2670c8bb155ffd42bff11
jawadsrour20 <67959735+jawadsrour20@users.noreply.github.com>
2022-04-09 15:19:54 +0300

    added post apis

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from http_status_codes import *
 from database import SessionLocal, engine
 from sqlalchemy.orm import Session
 import crud, models, schemas
-
+import utils
 # Create Database Tables
 models.Base.metadata.create_all(bind=engine)
 
@@ -53,6 +53,9 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     if is_username_taken:
         raise HTTPException(status_code=HTTP_STATUS_CODE_BAD_REQUEST, detail="Username already taken")
 
+    # create a submissions folder for the new student upon registration
+    utils.create_folder(user.username)
+    
     return crud.create_user(db=db, user=user)
 
 @app.post("/login")
@@ -81,13 +84,45 @@ def available_problems(user_info=Depends(auth_handler.auth_wrapper), db: Session
     overdue_submissions = crud.get_number_of_overdue_submissions(db, user_info["username"])
     average_grade = crud.get_average_grade(db, user_info["username"])
     problems = crud.get_problems_with_status(db, user_info["username"])
-    return {"average_grade": average_grade,
-            "overdue_submissions": overdue_submissions,
-            "problems": problems,
+    return {
+        "average_grade": average_grade,
+        "overdue_submissions": overdue_submissions,
+        "problems": problems,
+        "username": user_info["username"],
+        "name": user_info["name"],
+        "instructor_name": user_info["instructor_name"]
+        }
+
+@app.get("/problems/{problem_id}", status_code=HTTP_STATUS_CODE_OK)
+def available_problem(problem_id: int, user_info=Depends(auth_handler.auth_wrapper), db: Session = Depends(get_db)):
+
+    problem = crud.get_problem(db, problem_id)
+    return {"problem": problem,
             "username": user_info["username"],
             "name": user_info["name"],
             "instructor_name": user_info["instructor_name"]
             }
+
+@app.post("/problems/{problem_id}", status_code=HTTP_STATUS_CODE_CREATED)
+def submit_problem(problem_id: int, file: UploadFile = File(...), user_info=Depends(auth_handler.auth_wrapper), db: Session = Depends(get_db)):
+
+    username = user_info["username"]
+    student_id = crud.get_user_id(db, user_info["username"]).id
+    problem = crud.get_problem(db, problem_id)
+    problem_name = utils.get_file_name_without_extension(problem.file_name)
+    test_cases_file_name = f"test_{problem_name}.py"
+
+    with open(f"submissions/{username}/{file.filename}", "wb") as f:
+        shutil.copyfileobj(file.file, f)
+
+    # compare against pynguin test cases for the problem
+    # assign grade to submission + test cases passed + test cases failed
+    # store in database the results
+    grade = schemas.GradeCreate(student_id=student_id, problem_id=problem_id,
+                            test_cases_passed=0, test_cases_failed=0,
+                            grade_received=0)
+
+    return crud.add_grade(db=db, grade=grade)
 
 @app.post("/files")
 async def upload_files(files: List[UploadFile] = File(...), status_code=status.HTTP_201_CREATED):
@@ -100,7 +135,7 @@ async def upload_files(files: List[UploadFile] = File(...), status_code=status.H
 
         msg_list.append(pynguinAPI.run(file.filename))
         file_tests_dict[file.filename] = pynguinAPI.count_passed_and_failed_test_cases(file.filename)
-    # print(file_tests_dict)
+
     return {"msg": "Files uploaded successfully"}
     # return {"msg": msg_list}
 
--- utils.py
+++ utils.py
@@ -10,3 +10,20 @@ def get_os():
 # Returns True if the OS is MacOS or Linux, False otherwise (Windows)
 def is_unix():
     return get_os() == 'posix'
+
+def create_submissions_container_folder():
+    if not os.path.exists("submissions"):
+        os.mkdir("submissions")
+        print("Directory submissions created.")
+    else:
+        print("Directory submissions already exists")
+
+def create_folder(folder_name):
+
+    if not os.path.exists("submissions"):
+        create_submissions_container_folder()
+    try:
+        os.mkdir(f"submissions/{folder_name}")
+        print("Directory ", folder_name, " created.")
+    except FileExistsError:
+        print("Directory " , folder_name ,  " already exists")
